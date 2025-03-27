8ccee7b4803eab1c71eb6145eb5ffe7166c3643b
xdhruv27 <dhruv27x@gmail.com>
2025-03-27 10:10:15 +0530

    add token middleware for JWT verification and user ID extraction

---
+++ src/routes/middlewares/token-middleware.ts
@@ -0,0 +1,33 @@
+import { createMiddleware } from "hono/factory";
+import jwt from "jsonwebtoken";
+import { jwtsecretKey } from "../../../environment";
+
+export const tokenMiddleware = createMiddleware<{
+  Variables: {
+    userId: string;
+  };
+}>(async (context, next) => {
+  const token = context.req.header("token");
+  if (!token) {
+    return context.json(
+      {
+        message: "missing Token",
+      },
+      401
+    );
+  }
+
+  try {
+    const payload = jwt.verify(token, jwtsecretKey) as jwt.JwtPayload;
+
+    const userId = payload.sub;
+
+    if (userId) {
+      context.set("userId", userId);
+    }
+
+    await next();
+  } catch (e) {
+    return context.json({ message: "Unauthorized token" }, 401);
+  }
+});
